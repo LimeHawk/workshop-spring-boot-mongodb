d15de9fa6d5c645da62f35ecf56340a08708d8b6
LimeHawk <kaikelfalcao@gmail.com>
2022-12-01 21:46:46 -0300

    Consulta simples com query methods

--- src/main/java/com/neliosalves/workshopmongo/services/PostService.java
+++ src/main/java/com/neliosalves/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.neliosalves.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.neliosalves.workshopmongo.domain.Post;
-import com.neliosalves.workshopmongo.domain.User;
 import com.neliosalves.workshopmongo.repository.PostRepository;
 import com.neliosalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,6 +21,10 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
